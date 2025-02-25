1e4319177f0f561b09f4393f467460b2ee76bba0
Dora Deng <m1deng@ucsd.edu>
2025-02-25 11:36:51 -0800

    add class content

---
+++ class04/Class 04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 04 BIMM 143"
+#' author: "Dora Deng"
+#' date: "Jan 16th, 2025"
+#' ---
+
+# A plot from class 4. 
+x <- 1:50
+plot (x, sin(x))
+
+# Make this better
+plot(x, sin(x), col = "blue", type = "l", lwd=3)
